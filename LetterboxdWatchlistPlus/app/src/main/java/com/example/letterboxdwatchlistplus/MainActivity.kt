210607ed379c78179d862e2b46ba4f7350890f55
Samantha Bobra <sam.bobra@gmail.com>
2020-08-21 15:48:59 -0700

    Add links.

--- LetterboxdWatchlistPlus/app/src/main/java/com/example/letterboxdwatchlistplus/MainActivity.kt
+++ LetterboxdWatchlistPlus/app/src/main/java/com/example/letterboxdwatchlistplus/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+        // https://medium.com/@suragch/theme-appcompat-vs-themeoverlay-appcompat-in-android-c4abdc4d9683
+        // https://developer.android.com/training/appbar/setting-up
+        // https://medium.com/androiddevelopers/android-styling-themes-vs-styles-ebe05f917578
     }
 }
