86bc5fa8196d70169c9490ef76d9e2fe1b3027d2
Samantha Bobra <sam.bobra@gmail.com>
2020-08-21 17:59:30 -0700

    Add bottom navigation (not functional.)

--- LetterboxdWatchlistPlus/app/src/main/java/com/example/letterboxdwatchlistplus/MainActivity.kt
+++ LetterboxdWatchlistPlus/app/src/main/java/com/example/letterboxdwatchlistplus/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.letterboxdwatchlistplus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,7 @@ class MainActivity : AppCompatActivity() {
         // https://medium.com/@suragch/theme-appcompat-vs-themeoverlay-appcompat-in-android-c4abdc4d9683
         // https://developer.android.com/training/appbar/setting-up
         // https://medium.com/androiddevelopers/android-styling-themes-vs-styles-ebe05f917578
+        val navController = findNavController(R.id.nav_host_fragment)
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
     }
 }
